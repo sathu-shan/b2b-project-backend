295f6978cf73f6d7186a9bfce3b4c9719937f218
sathu-shan <sathushan@engenuityai.lk>
2023-09-13 10:16:17 +0530

    change the db

--- config/db.js
+++ config/db.js
@@ -1,17 +1,17 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('register', 'root', 'sathu0530A.', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
-
 // const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize('b2b_test', 'admin', 'Admin#123', {
-//   host: 'b2b-db-dev.cxas3mptqusg.us-east-1.rds.amazonaws.com',
+// const sequelize = new Sequelize('register', 'root', 'sathu0530A.', {
+//   host: 'localhost',
 //   dialect: 'mysql',
 // });
 
 // module.exports = sequelize;
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('b2b_test', 'admin', 'Admin#123', {
+  host: 'b2b-db-dev.cxas3mptqusg.us-east-1.rds.amazonaws.com',
+  dialect: 'mysql',
+});
+
+module.exports = sequelize;
